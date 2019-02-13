3ad9c2c87647f978c8873e4806b3f956302067e0
marcus fernando <marcusfcbarbosa@gmail.com>
2019-02-13 16:30:47 -0200

    COnfigurando Servidor Express

---
+++ backend/src/config/server.js
@@ -0,0 +1,14 @@
+const PORT = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+
+server.use(bodyParser.urlencoded({extended: true }))
+server.use(bodyParser.json())
+
+
+server.listen(PORT, function(){
+    console.log(`BACKEND IS RUNNING ON PORT ${PORT}`)
+})
