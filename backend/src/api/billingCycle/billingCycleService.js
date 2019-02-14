01ca889fdd91197b71415d3314ae7d6ccebe4984
marcus fernando <marcusfcbarbosa@gmail.com>
2019-02-14 15:30:09 -0200

    Sumarizando e retornando contador de cilcos de pagamento, SUMARIZADOR AINDA NAO ESTA FUNCIONANDO, DEVEgit add .! REFEAZERgit add .

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -5,6 +5,7 @@ billingCycle.methods(['get','post','put','delete'])
 //validações do dominio ODM
 billingCycle.updateOptions({ new:true, runValidators:true})
 
+//obtendo o contador de pagamentos
 billingCycle.route('count',(req,res,next)=>{
     billingCycle.count((error,value) =>{
         if(error){
@@ -14,5 +15,26 @@ billingCycle.route('count',(req,res,next)=>{
         }
     })
 })
+
+billingCycle.route('summary',(req,res,next)=>{
+    billingCycle.aggregate({
+        $project:{ credit:{$sum:"$credits.value"},debt:{$sum:"$debits.value"}}
+    },
+    {
+        $group:{ _id:null, credit:{$sum:"$credit"}, debt:{$sum:"$debt"}}
+    },{
+
+        $project:{ _id:0, credit:1, debt:1}
+    },(error,result)=>{
+        if(error){
+            res.status(500).json({erros:[error]})
+        }else{
+            res.json(result[0] || {credit:0,debt:0})
+        }
+    }
+   )
+})
+
+
 
 module.exports = billingCycle
