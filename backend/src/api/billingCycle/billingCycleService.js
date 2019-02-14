bf0c63f82e01f458ef7b29f23b0f8b190c1b3252
marcus fernando <marcusfcbarbosa@gmail.com>
2019-02-14 14:45:08 -0200

    Retornando um contador de BillingCyles (Pagamentos)

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -5,4 +5,14 @@ billingCycle.methods(['get','post','put','delete'])
 //validações do dominio ODM
 billingCycle.updateOptions({ new:true, runValidators:true})
 
+billingCycle.route('count',(req,res,next)=>{
+    billingCycle.count((error,value) =>{
+        if(error){
+            res.status(500).json({erros:[error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
+
 module.exports = billingCycle
