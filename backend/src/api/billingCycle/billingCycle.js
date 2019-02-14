5230ce3af265d887ee5c561bef19b623759c3624
marcus fernando <marcusfcbarbosa@gmail.com>
2019-02-14 14:10:52 -0200

    Fazendo as mensagens de validação padrão e validaçao especifica na ODM

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -2,22 +2,22 @@ const restfull = require('node-restful')//liga o express ao monoogose
 const mongoose = restfull.mongoose
 
 const creditSchema = new mongoose.Schema ({
-    name:{ type: String , required: true},
-    value: {type: Number, required: true}
+    name:{ type: String , required: [true, 'informe o nome do crédito']},
+    value: {type: Number, required: [true, 'Informe o valor do crédito']}
 })
 
 const debtSchema = new mongoose.Schema({
     name:{type:String, required: true},
-    value:{type:Number,min:0, required:true },
+    value:{type:Number,min:0, required:[true,'Informe o valor do Débito'] },
     status:{ type:String , required:false, uppercase:true ,
     enum:['PAGO','PENDENTE','AGENDADO']}
 })
 
 //Relação 1--->n
 const billingCycleSchema = new mongoose.Schema({
-    name:{type:String, required: true},
-    month:{type:Number, min:1, max:12, required:true},
-    year:{type: Number, min:1970, max:2100, required:true},
+    name:{type:String, required: [true,'Informe o nome do pagamento']},
+    month:{type:Number, min:1, max:12, required:[true,'Informe o mês']},
+    year:{type: Number, min:1970, max:2100, required:[true,'Informe o ano do pagamento']},
     credits:[creditSchema],
     debits:[debtSchema]
 })
