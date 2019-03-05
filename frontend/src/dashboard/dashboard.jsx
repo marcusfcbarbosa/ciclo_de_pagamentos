eda7adbae4ebdfc088201351ffce970020e99f3e
marcus fernando <marcusfcbarbosa@gmail.com>
2019-03-04 23:04:36 -0300

    Configurando o layout de tela para responsividade

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -5,6 +5,7 @@ import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 
 import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class DashBoard extends Component{
     render(){
@@ -12,27 +13,31 @@ class DashBoard extends Component{
                 <div>
                     <ContentHeader tittle='Dashboard'  subtittle='Versão 1.0'/>
                     <Content>
-                        <ValueBox 
-                        cols='12 4'
-                        color='green'
-                        icon='bank'
-                        value='R$ 10'
-                        text='Total de créditos'
-                        />
-                        <ValueBox 
-                        cols='12 4'
-                        color='red'
-                        icon='credit-card'
-                        value='R$ 10'
-                        text='Total de débitos'
-                        />
-                        <ValueBox 
-                        cols='12 4'
-                        color='blue'
-                        icon='money'
-                        value='R$ 0'
-                        text='Valor Consolidado'
-                        />
+                        <Row>
+                            <ValueBox 
+                            cols='12 4'
+                            color='green'
+                            icon='bank'
+                            value='R$ 10'
+                            text='Total de créditos'
+                            />
+                            <ValueBox 
+                            cols='12 4'
+                            color='red'
+                            icon='credit-card'
+                            value='R$ 10'
+                            text='Total de débitos'
+                            />
+                            <ValueBox 
+                            cols='12 4'
+                            color='blue'
+                            icon='money'
+                            value='R$ 0'
+                            text='Valor Consolidado'
+                            />
+
+                        </Row>
+                        
                     </Content>
                 </div>
         )
