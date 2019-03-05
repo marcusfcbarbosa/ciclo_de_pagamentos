f09d2ab4de8a0552f1c1ca217560ee768594b04d
marcus fernando <marcusfcbarbosa@gmail.com>
2019-03-04 21:59:54 -0300

    Migrando o DashBoard para um componente em classe e criando dois componente de Content e ContentHeader dentro de DashBoard

---
+++ frontend/src/common/template/contentHeader.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// Dados extraidos do template LteAdmin
+export default props => (
+    <section className='content-header'>
+        <h1> { props.tittle } <small> {props.subtittle} </small> </h1>       
+    </section>
+)
