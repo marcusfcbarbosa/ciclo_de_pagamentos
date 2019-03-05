a89642ae19a7bed0b65a963a8b53903c3985dd73
marcus fernando <marcusfcbarbosa@gmail.com>
2019-03-04 22:58:04 -0300

    Usando o ValueeBox dentro do Dashboard

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -4,13 +4,35 @@ import React, {Component} from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 
+import ValueBox from '../common/widget/valueBox'
+
 class DashBoard extends Component{
     render(){
         return(
                 <div>
                     <ContentHeader tittle='Dashboard'  subtittle='Versão 1.0'/>
                     <Content>
-                        DashBoard
+                        <ValueBox 
+                        cols='12 4'
+                        color='green'
+                        icon='bank'
+                        value='R$ 10'
+                        text='Total de créditos'
+                        />
+                        <ValueBox 
+                        cols='12 4'
+                        color='red'
+                        icon='credit-card'
+                        value='R$ 10'
+                        text='Total de débitos'
+                        />
+                        <ValueBox 
+                        cols='12 4'
+                        color='blue'
+                        icon='money'
+                        value='R$ 0'
+                        text='Valor Consolidado'
+                        />
                     </Content>
                 </div>
         )
