bf7a80f945c51fb1e0bb55e238596361d86f4e6c
marcus fernando <marcusfcbarbosa@gmail.com>
2019-03-04 21:42:17 -0300

    Definindo as rotas de Dashboard e BillingCycle

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props =>(
+    <footer className='main-footer'>
+        <strong>
+            Copyright & Copy 2019
+            <a href='https://github.com/marcusfcbarbosa' targe='_blank'> Marcus Barbosa</a>.
+        </strong>
+    </footer>
+)
