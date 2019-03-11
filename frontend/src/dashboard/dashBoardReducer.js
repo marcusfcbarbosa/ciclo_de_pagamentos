61a53cc217664e5271808a44cd9341eea1a382b2
marcus fernando <marcusfcbarbosa@gmail.com>
2019-03-11 17:02:45 -0300

    pasando a integração do dashBoard, usando o DashBpardReducer para o Reducer

--- frontend/src/dashboard/dashBoardReducer.js
+++ frontend/src/dashboard/dashBoardReducer.js
@@ -1,4 +1,4 @@
-const INITIAL_STATE = { summary :{ credit:0, debt:0 }}
+const INITIAL_STATE = { summary :{ credit:1000, debt:530 }}
 
 export default function(state = INITIAL_STATE, action){
     return state
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
 
-//import DashBoardReducer from '../dashboard/dashBoardReducer'
+import DashBoardReducer from '../dashboard/dashBoardReducer'
 
 
 const rootReducer = combineReducers({
-    dashboard: () => ({ summary: { credit:100, debt:50 }})
+    dashboard: DashBoardReducer
 })
 
 export default rootReducer
