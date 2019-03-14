6856553debed0eee8c788122754187918416236b
marcus fernando <marcusfcbarbosa@gmail.com>
2019-03-13 22:19:28 -0300

    Alterando o metodo de agregação, porém ainda nao retornando com o valor correto

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -16,19 +16,21 @@ BillingCycle.route('count', (req, res, next) => {
 })
 
 BillingCycle.route('summary', (req, res, next) => {
-    BillingCycle.aggregate({
-        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
-    }, {
-        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
-    }, {
-        $project: {_id: 0, credit: 1, debt: 1}
-    }, (error, result) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
-        } else {
-            res.json(result[0] || { credit: 0, debt: 0 })
-        }
-    })
+
+      const cursor = BillingCycle.aggregate()
+        .project({ credito: { $sum: "$credits.value" }, debito: { $sum: "$debts.value" } })
+        .group({ _id: null, credito: { $sum: "$credit" }, debito: { $sum: "$debt" } })
+        .project({ _id: 0, credito: 1, debito: 1 })
+        .cursor({ batchSize: 100 })
+        .exec()
+
+        cursor.get((error, result) => {
+          if (error) {
+              res.status(500).json({ errors: [error] })
+          } else {
+              res.json(result[0] || { credito: 0, debito: 0 })
+          }
+        })
 })
 
 module.exports = BillingCycle
