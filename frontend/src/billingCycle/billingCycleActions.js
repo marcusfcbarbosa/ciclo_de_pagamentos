0c6efaa921a5bdf53a0136140e9b2acca6c12228
marcus fernando <marcusfcbarbosa@gmail.com>
2019-03-20 22:23:39 -0300

    após enviar um post ele limpa o formulario, recarrega a lista, pre seleciona a tabList e exibe as duas abas de listagem e criacao

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'//para as chamadas http
 import { toastr } from 'react-redux-toastr'//adicionando as mensagens de sucesso ou de erro
+import { reset as resetForm } from 'redux-form'
+import { showTabs,selectTab  } from '../common/tab/tabActions'
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList(){
@@ -11,13 +14,24 @@ export function getList(){
 }
 //e.response.data (os dados que vieram da resposta)
 export function create(values){
-    const request = axios.post(`${BASE_URL}/billingCycles`,values)
-                    .then(resp=>{
-                        toastr.success('Sucesso','Operação Realizada com sucesso.')
-                    })
-                    .catch(e =>{
-                        e.response.data.errors.forEach(error=>toastr.error('Erro',error))
-                    })
+
+        return dispach=>{
+            axios.post(`${BASE_URL}/billingCycles`,values)
+            .then(resp=>{
+                toastr.success('Sucesso','Operação Realizada com sucesso.')
+                dispach([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList','tabCreate')
+                ])
+            })
+            .catch(e =>{
+                e.response.data.errors.forEach(error=>toastr.error('Erro',error))
+            })
+        }
+
+    
     return{
         type:'TEMP',
         payload:request
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,12 +5,15 @@ import { Provider } from 'react-redux'//Ele pega seu estado e leva aos component
 
 
 import promise  from 'redux-promise'//para trabalhar com requisição assincrona
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
+
 import App from './main/app'
 import reducers from './main/reducers'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
 //preparando minha store para trabalhar com requisições assincronas, o applyMiddleware espera a resolução da promise, para poder carregar o .data
-const store = applyMiddleware(promise)(createStore)(reducers,devTools)
+const store = applyMiddleware(promise,multi,thunk)(createStore)(reducers,devTools)
 
 ReactDOM.render(
    <Provider store={store}>
